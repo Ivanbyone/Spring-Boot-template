f78bddee413da76c57c7f1f21acfcea7773f2b87
ivanbyone <ivanbyone@gmail.com>
2024-11-20 20:38:37 +0300

    Fixed bug with gateways

--- src/main/java/com/forms_example/Spring_Forms_rest_example/application/gateways/IFeedbackFormGateway.java
+++ src/main/java/com/forms_example/Spring_Forms_rest_example/application/gateways/IFeedbackFormGateway.java
@@ -1,7 +1,6 @@
 package com.forms_example.Spring_Forms_rest_example.application.gateways;
 
 import com.forms_example.Spring_Forms_rest_example.domain.common.IFeedbackForm;
-import org.bson.Document;
 
 public interface IFeedbackFormGateway {
     String addForm(IFeedbackForm form);
