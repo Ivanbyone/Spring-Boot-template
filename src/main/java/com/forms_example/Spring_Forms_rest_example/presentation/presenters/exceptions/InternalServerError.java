02c0afdcdd886fd19fdf0316a4c0d79061ed24dc
ivanbyone <ivanbyone@gmail.com>
2024-11-21 10:10:29 +0300

    fixed bug with 500 error

--- src/main/java/com/forms_example/Spring_Forms_rest_example/presentation/presenters/exceptions/InternalServerError.java
+++ src/main/java/com/forms_example/Spring_Forms_rest_example/presentation/presenters/exceptions/InternalServerError.java
@@ -1,6 +1,10 @@
 package com.forms_example.Spring_Forms_rest_example.presentation.presenters.exceptions;
 
-public class InternalServerError extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Internal Server Error")
+public class InternalServerError extends Exception {
     public InternalServerError(String message) {
         super(message);
     }
